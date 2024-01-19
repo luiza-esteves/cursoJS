bd0c8de9824ceb768d2131223677428fbe82fc7b
Luíza Esteves <luizaesteves987@gmail.com>
2024-01-19 15:52:56 -0300

    manipulando valores

--- dom/script.js
+++ dom/script.js
@@ -40,3 +40,54 @@ for (let element of classname){
 }
 
 const tag= document.getElementsByTagName('p')
+
+/* ---------------------------------------------------
+ MANIPULANDO VALORES
+
+  - innerText e textContent
+InnerText retorna só o texto sem formatação
+textContent retorna o texto com formatação e elementos HTML
+
+*/
+
+const p5 = document.querySelector('p');
+console.log(p5.textContent);
+console.log(p5.innerText);
+
+/* ---------------------------------------------------
+  - innerHTML e createElement
+innerHTML retorna o texto com formatação e elementos html
+createElement cria um elemento html
+
+*/
+const div = document.querySelector('div');
+console.log(1,div);
+div.innerHTML = "<strong> Texto alterado</strong>"
+const element1 = document.createElement('ul');
+[1,2,3].forEach((element)=>{
+    const elementLi = document.createElement('li');
+    elementLi.innerText = element;
+    element1.appendChild(elementLi)
+});
+console.log(element1);
+div.appendChild(element1);
+
+/* ---------------------------------------------------
+  - inputValue
+*/
+const input = document.querySelector('#name');
+
+
+setTimeout(()=>{
+    input.value="Luíza";
+    console.log(input.value);
+},1000)
+
+/* ---------------------------------------------------
+  - atributos
+*/
+
+const teste = document.querySelector('div');
+div.setAttribute("id","teste");
+div.setAttribute("class",`${div.getAttribute("class")} classe2`);
+div.removeAttribute("class");
